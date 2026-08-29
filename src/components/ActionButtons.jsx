35a5985349f802dfca7c149d7c19f9cdc0078649
yujizcat2 <yujizcat@gmail.com>
2026-08-29 20:03:48 +0900

    fix: restore combine action in simple eight palace

--- src/components/ActionButtons.jsx
+++ src/components/ActionButtons.jsx
@@ -71,7 +71,7 @@ export default function ActionButtons({
 
         onClick={
           canCombine && !preview?.combine?.requiresTypeChoice
-            ? onCombine
+            ? () => onCombine()
             : undefined
         }
 
