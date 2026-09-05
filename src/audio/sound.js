ed2a08d42266d5cf3ffd4ef8e217cde8f7602586
yujizcat2 <yujizcat@gmail.com>
2026-09-06 00:27:33 +0900

    tune: increase game sound volume

--- src/audio/sound.js
+++ src/audio/sound.js
@@ -14,7 +14,7 @@ function getAudioContext(){
   if(!audioContext){
     audioContext = new AudioContext();
     masterGain = audioContext.createGain();
-    masterGain.gain.value = 0.6;
+    masterGain.gain.value = 0.75;
     masterGain.connect(audioContext.destination);
   }
 
